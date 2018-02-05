eb098e06de193e603bd01d7bdf44369f8eecdf2c
andorsk <andor.developer@gmail.com>
2018-02-05 16:10:34 -0300

    working on abstracting local classes to appropriate folders

--- src/public/js/helpers.js
+++ src/public/js/helpers.js
@@ -1,13 +1,12 @@
+var loadedscripts = {};
 
- var loadedscripts = {};
-
- //fill helper for multi deminsional arrays. fills it with val.
- function fill(mat, val){
-   for(var i = 0; i < mat.length; i++){
-     mat[i] = mat[i].fill(val)
-   }
-   return mat;
- }
+//fill helper for multi deminsional arrays. fills it with val.
+function fill(mat, val) {
+    for (var i = 0; i < mat.length; i++) {
+        mat[i] = mat[i].fill(val)
+    }
+    return mat;
+}
 
 
 var contains = function(needle) {
@@ -15,16 +14,17 @@ var contains = function(needle) {
     var findNaN = needle !== needle;
     var indexOf;
 
-    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+    if (!findNaN && typeof Array.prototype.indexOf === 'function') {
         indexOf = Array.prototype.indexOf;
     } else {
         indexOf = function(needle) {
-            var i = -1, index = -1;
+            var i = -1,
+                index = -1;
 
-            for(i = 0; i < this.length; i++) {
+            for (i = 0; i < this.length; i++) {
                 var item = this[i];
 
-                if((findNaN && item !== item) || item === needle) {
+                if ((findNaN && item !== item) || item === needle) {
                     index = i;
                     break;
                 }
@@ -38,76 +38,75 @@ var contains = function(needle) {
 };
 
 
- function clone(mat){
+function clone(mat) {
     return $.extend(true, [], mat);
- }
- 
+}
+
 //https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
 }
 
 
-Array.prototype.insertIntoEach = function(val){
-     for(var i = 0; i < this.length; i++){
-      var copyOfMyArray = $.extend(true, [], val);   //need to make a copy or else array will update multiple points.   
-      this[i] = copyOfMyArray;
-     }
+Array.prototype.insertIntoEach = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        var copyOfMyArray = $.extend(true, [], val); //need to make a copy or else array will update multiple points.   
+        this[i] = copyOfMyArray;
+    }
 }
 
- //fill helper for multi deminsional arrays. fills it with val.
- function zeroarray(size){
-   var arr = new Array(size)
-   for(var i = 0; i < arr.length; i++){
-     arr[i] = arr[i].fill(0)
-   }
-   return arr;
- }
- //convert an index to a location on a board.
-  function convertindextoloc(board, index){
-        var rows = Math.floor(index / board.size()[1])
-        var columns = index % board.size()[1]
-        return [rows, columns]
-  }
+//fill helper for multi deminsional arrays. fills it with val.
+function zeroarray(size) {
+    var arr = new Array(size)
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].fill(0)
+    }
+    return arr;
+}
+//convert an index to a location on a board.
+function convertindextoloc(board, index) {
+    var rows = Math.floor(index / board.size()[1])
+    var columns = index % board.size()[1]
+    return [rows, columns]
+}
 
-  function sendMessage(selector, message){
-      $(selector).find("#message").text(message)
-  }
+function sendMessage(selector, message) {
+    $(selector).find("#message").text(message)
+}
 
-  //execute a function by the name
-  function executeFunctionByName(functionName, context /*, args */) {
+//execute a function by the name
+function executeFunctionByName(functionName, context /*, args */ ) {
     var args = Array.prototype.slice.call(arguments, 2);
     var namespaces = functionName.split(".");
     var func = namespaces.pop();
-    for(var i = 0; i < namespaces.length; i++) {
-      context = context[namespaces[i]];
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
     }
     return context[func].apply(context, args);
-  }
-  //Load Scripts required to get all the necessary javascript scripts loaded. Better way of handling this later. 
-function loadScripts(){
-  var scripts = [
-    '/js/config.js',
-    '/js/game.js',
-    '/js/objects.js',
-    '/js/agents.js',
-    '/js/valueiteration.js',
-    '/js/test.js',
-  ]
+}
+//Load Scripts required to get all the necessary javascript scripts loaded. Better way of handling this later. 
+function loadScripts() {
+    var scripts = [
+        '/js/config.js',
+        '/js/objects.js',
+        '/js/agents.js',
+        '/js/valueiteration.js',
+        '/js/test.js',
+    ]
 
-  for(var i =0; i < scripts.length; i++){
-    var script = scripts[i];
-    loadScript(script, registerScript)
-  }
+    for (var i = 0; i < scripts.length; i++) {
+        var script = scripts[i];
+        loadScript(script, registerScript)
+    }
 }
 
-function exit( status ) {
+function exit(status) {
     // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brettz9.blogspot.com)
     // +      input by: Paul
@@ -124,7 +123,10 @@ function exit( status ) {
         alert(status);
     }
 
-    window.addEventListener('error', function (e) {e.preventDefault();e.stopPropagation();}, false);
+    window.addEventListener('error', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }, false);
 
     var handlers = [
         'copy', 'cut', 'paste',
@@ -133,12 +135,14 @@ function exit( status ) {
         'abort', 'close', 'dragdrop', 'load', 'paint', 'reset', 'select', 'submit', 'unload'
     ];
 
-    function stopPropagation (e) {
+    function stopPropagation(e) {
         e.stopPropagation();
         // e.preventDefault(); // Stop for the form controls, etc., too?
     }
-    for (i=0; i < handlers.length; i++) {
-        window.addEventListener(handlers[i], function (e) {stopPropagation(e);}, true);
+    for (i = 0; i < handlers.length; i++) {
+        window.addEventListener(handlers[i], function(e) {
+            stopPropagation(e);
+        }, true);
     }
 
     if (window.stop) {
@@ -147,46 +151,47 @@ function exit( status ) {
 
     throw '';
 }
-  function registerScript(url){
-    loadedscripts[url] = "true";
-  }
 
-  //checks if script is loaded. if not, it will hold to a timing pattern until it is. timeout is set default to 3 sec.
-  //Note: For now I just force load of url if it is not loaded synchronously. 
-  function waitUntilScriptLoaded(url){
+function registerScript(url) {
+    loadedscripts[url] = "true";
+}
+
+//checks if script is loaded. if not, it will hold to a timing pattern until it is. timeout is set default to 3 sec.
+//Note: For now I just force load of url if it is not loaded synchronously. 
+function waitUntilScriptLoaded(url) {
     var defaultTimeout = 3000;
-    if(isScriptLoaded(url)){
-      return;
-    } else{
+    if (isScriptLoaded(url)) {
+        return;
+    } else {
         console.log("Forcing load of URL" + url);
         loadURLSynchronously(url);
-      }
-    } 
-  
-  function loadURLSynchronously(url){
-    $.ajax({
-      async: false,
-      url: url,
-      dataType: "script"
-  });
-  }
-
-  function isScriptLoaded(url){
-    if(url in loadedscripts){ 
-      return true
-    } else{
-      return false;
     }
-  }
+}
 
-  /**
-  Load the javascript first by the url. 
-  **/
-  function loadScript(url, callback){
+function loadURLSynchronously(url) {
     $.ajax({
-      url: url,
-      dataType: "script",
-    }).done(function(){
-      callback(url);
+        async: false,
+        url: url,
+        dataType: "script"
     });
-  }
+}
+
+function isScriptLoaded(url) {
+    if (url in loadedscripts) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+/**
+Load the javascript first by the url. 
+**/
+function loadScript(url, callback) {
+    $.ajax({
+        url: url,
+        dataType: "script",
+    }).done(function() {
+        callback(url);
+    });
+}
